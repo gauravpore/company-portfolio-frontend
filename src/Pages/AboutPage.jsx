53b590692218a0e64a170a5d738486cdcb677e3a
sachin3825 <sachinkumawat55@gmail.com>
2023-07-01 13:18:51 +0530

    about us page completed

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import HeroImage from "../Components/UI components/HeroImage";
 import AboutImage from "../Assets/AboutUsImage.jpg";
 import Title from "../Components/UI components/TitleComponent";
-
-import LogoCarousel from "../Components/HomePage components/LogoCarousel";
-
+import AboutContent from "../Components/AboutPage content/AboutContent";
 const AboutPage = () => {
   return (
     <div>
       <HeroImage imageUrl={AboutImage}>
         <Title text={"About us"} />
       </HeroImage>
-      <LogoCarousel />
+      <AboutContent />
     </div>
   );
 };
